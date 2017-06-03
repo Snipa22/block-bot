a6b1303bdf2958b4680c1e6b373ef190c4939ed7
Alexander Blair <alex@snipanet.com>
2017-06-02 22:35:14 -0700

    I like timestamps.

--- bot.js
+++ bot.js
@@ -18,8 +18,9 @@ function nodejs_pool_bot(site, url){
             if (body.length > 0 && body[0].height > config.hosts[site].last_block_id){
                 debug(site + " is at height: " + body[0].height);
                 if (config.hosts[site].last_block_id !== 0){
+                    let utc_find = new Date(body[0].ts);
                     config.irc.config.channels.forEach(function(channel){
-                        let text_string = "Block " + body[0].height.toString() + " found on "+site+" approximately "+ (Math.floor(Date.now() / 1000) - Math.floor(body[0].ts / 1000)).toString() + " seconds ago on the " + body[0].pool_type + " pool!";
+                        let text_string = "Block " + body[0].height.toString() + " found on "+site+" approximately "+ (Math.floor(Date.now() / 1000) - Math.floor(body[0].ts / 1000)).toString() + " seconds ago at "+utc_find.toISOString()+" on the " + body[0].pool_type + " pool!";
                         bot.say(channel, text_string);
                     });
                 }
@@ -48,10 +49,11 @@ function handle_cn_data(err, res, site){
                 }
                 if (blocks.length === 0){
                     debug(site + " is at height: " + best_block.toString());
-                    if (block_height > config.hosts[site].last_block_id && config.hosts[site].last_block_id !== 0){
+                    if (best_block > config.hosts[site].last_block_id && config.hosts[site].last_block_id !== 0){
                         let block_data = best_block_data.split(':');
+                        let utc_find = new Date(parseInt(block_data[1])*1000);
                         config.irc.config.channels.forEach(function(channel){
-                            let text_string = "Block " + block_height.toString() + " found on "+site+" approximately "+ (Math.floor(Date.now() / 1000) - parseInt(block_data[1])).toString() + " seconds ago!";
+                            let text_string = "Block " + best_block.toString() + " found on "+site+" approximately "+ (Math.floor(Date.now() / 1000) - parseInt(block_data[1])).toString() + " seconds ago at "+utc_find.toISOString()+"!";
                             bot.say(channel, text_string);
                         });
                     }
